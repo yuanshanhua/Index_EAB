e6d81a0ed966ae54fd4ded15f5b73eee1a23ddac
yuanshanhua <187101411+yuanshanhua@users.noreply.github.com>
2025-08-11 15:05:55 +0800

    添加命令示例

--- heu_run.py
+++ heu_run.py
@@ -295,6 +295,14 @@ def get_heu_result(args, algos, work_list):
 
 
 if __name__ == "__main__":
+    """
+    uv run heu_run.py --work_file tpch_2000.sql \
+    --res_save tpch_2000.json \
+    --algo extend \
+    --exp_conf_file configuration_loader/index_advisor/heu_run_conf/extend_config.json \
+    --schema_file configuration_loader/database/schema_tpch.json \
+    --db_conf_file configuration_loader/database/db_con.conf
+    """
     print("=== 启动索引建议器程序 ===")
 
     parser = get_parser()
